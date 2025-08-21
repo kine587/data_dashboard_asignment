ec087daf2dc7bf503f03cd73c0cc0da6ff84c279
kine <kine.kodehode@gmail.com>
2025-08-21 14:25:52 +0200

    began on javascript part of the assignment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-JSON 
-{
-    "name": "one piece"
-    autor: "Eiichiro Oda"
-    genere:
-}
+JSON;
+
+movies = [];
+series = [];
+
+const title = document.getElementById("title");
+const autor = document.getElementById("autor");
+const genere = document.getElementById("genere");
+const episodes = document.getElementById("episodes");
